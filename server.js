2a811ce6cb1ecc1a2a2fe2398e930e3f0a87a9a1
Sam Ardis <ardis.productions@gmail.com>
2020-04-08 16:26:02 -0400

    Added index.js, and server.js code for now, starting on userouter

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter');
+
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.use('/api/users', userRouter);
+server.get("/", logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use(logger);
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.url);
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get(
+      "Origin"
+    )}`
+  );
+  next();
+}
 
 module.exports = server;
